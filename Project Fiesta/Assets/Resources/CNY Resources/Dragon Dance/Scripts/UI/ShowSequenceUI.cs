b59d3425b4cd059d1c7aeb2aff2c33d41bcf8aa6
SleepyWolfy <inaki.2012@hotmail.com>
2020-09-17 11:15:32 -0300

    Change to support SingleUI in showing sequence

--- Project Fiesta/Assets/Resources/CNY Resources/Dragon Dance/Scripts/UI/ShowSequenceUI.cs
+++ Project Fiesta/Assets/Resources/CNY Resources/Dragon Dance/Scripts/UI/ShowSequenceUI.cs
@@ -11,56 +11,61 @@ namespace FiestaTime
     {
         public class ShowSequenceUI : MonoBehaviour
         {
-            [SerializeField] private RectTransform[] arrowImages;
-            [SerializeField] private GameObject[] arrows;
+            [SerializeField] private RectTransform arrowImage;
+            [SerializeField] private GameObject arrow;
 
             //[SerializeField] private Image rightArrowImage;
             //[SerializeField] private Image upArrowImage;
             //[SerializeField] private Image leftArrowImage;
             //[SerializeField] private Image downArrowImage;
 
-            // Start is called before the first frame update
-            void Start()
-            {
-                SetArrows();
-            }
-
             private void OnEnable()
             {
-                IndicatorFunctions.DisableAllIndicators(arrows);
                 StartCoroutine(ShowSequence(GameManager.Current.amountOfMovesThisRound));
             }
 
             private void OnDisable()
             {
-                IndicatorFunctions.DisableAllIndicators(arrows);
+                
             }
 
-            private void SetArrows()
+            private void SetArrow(int sequence)
             {
-                for (int i = 0; i < GameManager.Current.sequenceMap.Length; i++)
-                {
-                    int rotation = 270;
-                    if (GameManager.Current.sequenceMap[i] == 2) rotation = 0;
-                    if (GameManager.Current.sequenceMap[i] == 3) rotation = 90;
-                    if (GameManager.Current.sequenceMap[i] == 4) rotation = 180;
+                int rotation = 270;
+                if (sequence == 2) rotation = 0;
+                if (sequence == 3) rotation = 90;
+                if (sequence == 4) rotation = 180;
 
-                    arrowImages[i].Rotate(new Vector3(0, 0, rotation));
-                }
+                arrowImage.Rotate(new Vector3(0, 0, rotation));
             }
 
             private IEnumerator ShowSequence(int elements)
             {
-                yield return new WaitForSeconds(1f);
-
                 for(int i = 0; i < elements; i++)
                 {
-                    arrows[i].SetActive(true);
+                    arrowImage.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
-                    yield return new WaitForSeconds(0.5f);
+                    SetArrow(GameManager.Current.sequenceMap[i]);
+
+                    arrow.SetActive(true);
+
+                    StartCoroutine(AnimateArrowCo());
+
+                    yield return new WaitForSeconds(1f);
                 }
+                arrow.SetActive(false);
 
                 GameManager.Current.NotifyOfPlayerReady(PhotonNetwork.LocalPlayer.ActorNumber);
+            }
+
+            private IEnumerator AnimateArrowCo()
+            {
+                for(int i = 0; i < 70; i++)
+                {
+                    arrowImage.localScale += new Vector3(0.00714285714f, 0.00714285714f, 0.00714285714f);
+
+                    yield return new WaitForSeconds(0.01f);
+                }
             }
         }
     }
