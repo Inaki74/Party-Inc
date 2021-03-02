b3d880992c24d3c21d69dcce65cb7681d9e55e98
SleepyWolfy <inaki.2012@hotmail.com>
2021-03-02 19:10:12 -0300

    KK: Created project directory and basic scripts
    Basic GameManager, basic player controller and input, basic camera.

---
+++ Party-Inc/Assets/PartyInc/Resources/2_Games/9_KaveKerfuffle/_Scripts/_general/Mng_GameManager_KK.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PartyInc
+{
+    namespace KK
+    {
+        public class Mng_GameManager_KK : MonoSingleton<Mng_GameManager_KK>
+        {
+            [SerializeField] private float _movementSpeed;
+            public float MovementSpeed
+            {
+                get
+                {
+                    return _movementSpeed;
+                }
+                private set
+                {
+                    _movementSpeed = value;
+                }
+            }
+
+            // Start is called before the first frame update
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+        }
+    }
+}
+
+
---
+++ Party-Inc/Assets/PartyInc/Resources/2_Games/9_KaveKerfuffle/_Scripts/_player/Mono_Player_Controller_KK.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PartyInc
+{
+    namespace KK
+    {
+        public class Mono_Player_Controller_KK : MonoBehaviour
+        {
+            [SerializeField] private Rigidbody _rb;
+            [SerializeField] private Mono_Player_Input_KK _input;
+
+            [SerializeField] private float _force;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                if(_rb == null)
+                {
+                    _rb = GetComponent<Rigidbody>();
+                }
+            }
+
+            private void FixedUpdate()
+            {
+                if (_input.JumpInput)
+                {
+                    _input.JumpInput = false;
+                    _rb.velocity = Vector3.zero;
+                    _rb.AddForce(Vector3.up * _force, ForceMode.Impulse);
+                }
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                transform.position += Vector3.right * Mng_GameManager_KK.Current.MovementSpeed * Time.deltaTime;
+
+                
+            }
+        }
+    }
+}
+
+
